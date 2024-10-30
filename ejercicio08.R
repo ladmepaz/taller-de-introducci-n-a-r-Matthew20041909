fe1828e5f6a79fa2486b5b8c245ddac22040a2ca
Matthew20041909 <mapalominoc@unal.edu.co>
2024-10-30 08:50:02 -0500

    Update ejercicio08.R

--- ejercicio08.R
+++ ejercicio08.R
@@ -1,6 +1,6 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function() {
+solucion <- function(c) {
   # Ingresa valores en las líneas indicadas de manera que a 's' se le asigne 4
   # y a 't' se le asigne 10
   a <- 4
