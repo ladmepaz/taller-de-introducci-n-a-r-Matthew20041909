7300d3419ac535794768ce479d4406b21dfde5cd
Matthew20041909 <mapalominoc@unal.edu.co>
2024-10-30 07:36:33 -0500

    Update ejercicio07.R

--- ejercicio07.R
+++ ejercicio07.R
@@ -1,11 +1,11 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function() {
+solucion <- function(s) {
   # Escribe una expresión de manera que a 's' se le asigne el promedio de 'a', 'b', 'c', y 'd'
   a <- 4
   b <- 8
   c <- 5
   d <- 3
-  s <- NULL # ingresa expresión aquí
+  s <- (a+b+c+d)/4 # ingresa expresión aquí
   return(s)
 }
