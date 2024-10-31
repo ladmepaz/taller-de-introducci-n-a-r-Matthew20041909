7f2eca3ec07dbc963c71108958723ee4a7e2e3c5
Matthew20041909 <mapalominoc@unal.edu.co>
2024-10-31 07:04:19 -0500

    Update ejercicio01.R

--- ejercicio01.R
+++ ejercicio01.R
@@ -1,6 +1,6 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function() {
+solucion <- function(s) {
   # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
   a <- 68 # asigna valor aquí
   b <- 32 # asigna valor aquí
